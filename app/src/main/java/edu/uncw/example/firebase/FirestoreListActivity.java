d994db0872bc413d0f044ab60f82065cea8be481
Lucas Layman <laymanl@uncw.edu>
2021-10-26 14:55:56 -0400

    FirestoreListActivity add and get only.

--- app/src/main/java/edu/uncw/example/firebase/FirestoreListActivity.java
+++ app/src/main/java/edu/uncw/example/firebase/FirestoreListActivity.java
@@ -1,19 +1,13 @@
 package edu.uncw.example.firebase;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -27,7 +21,7 @@ import java.util.ArrayList;
 
 public class FirestoreListActivity extends AppCompatActivity {
 
-    private FirebaseFirestore mDb = FirebaseFirestore.getInstance();
+    private final FirebaseFirestore mDb = FirebaseFirestore.getInstance();
     private static final String TAG = "FirestoreListActivity";
     private static final String PATIENTS = "patients";
 
@@ -38,41 +32,6 @@ public class FirestoreListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firestore_list);
 
-        ListView patientListView = findViewById(R.id.patientListView);
-//        adapter = new ArrayAdapter<Patient>(
-//                this,
-//                android.R.layout.simple_list_item_1,
-//                new ArrayList<Patient>()
-//        );
-        adapter = new PatientAdapter(this, new ArrayList<Patient>());
-        patientListView.setAdapter(adapter);
-
-    }
-
-    class PatientAdapter extends ArrayAdapter<Patient> {
-
-        ArrayList<Patient> patients;
-        PatientAdapter(Context context, ArrayList<Patient> patients) {
-            super(context, 0, patients);
-            this.patients = patients;
-        }
-
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            if (convertView == null) {
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.patient_list_item, parent, false);
-            }
-
-            TextView patientName = convertView.findViewById(R.id.itemName);
-            TextView patientAge = convertView.findViewById(R.id.itemAge);
-
-            Patient p = patients.get(position);
-            patientName.setText(p.getName());
-            patientAge.setText(Integer.toString(p.getAge()));
-
-            return convertView;
-        }
     }
 
     public void onRefreshClick(View view) {
@@ -87,9 +46,6 @@ public class FirestoreListActivity extends AppCompatActivity {
                             patients.add(p);
                             Log.d(TAG, p.getName() + " " + p.getAge());
                         }
-                        adapter.clear();
-                        adapter.addAll(patients);
-
                     }
                 });
     }
